e468d387a799025ac206c9fb5f3eaef48f22e6b3
ysr <ysrcoder@gmail.com>
2025-11-28 23:47:51 -0500

    Clean building files

--- src/build/turtlebot3_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/justin/sim_ws/src/turtlebot3_gazebo
-# Build directory: /home/justin/sim_ws/src/build/turtlebot3_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
